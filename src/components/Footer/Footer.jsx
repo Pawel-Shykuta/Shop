cac2a9bc938236dee6908e6fed3c7db355f33b62
Pawel Shykuta <pawelshikuta@gmail.com>
2025-10-31 18:08:02 +0100

    Fix Blog Secton

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,20 +3,27 @@ import Styles from './FooterStyle.module.scss'
 import { CiInstagram } from "react-icons/ci";
 import { FiFacebook } from "react-icons/fi";
 import { FaYoutube } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 const Footer = () =>{
+    const navigate = useNavigate()
+
+    const newChangeLocation = (name) =>{
+        navigate(name)
+        window.scrollTo({top:0, behavior:'smooth'})
+    }
 
     return(
         <footer className={Styles.Footer_Wrapper}>
             <section className={Styles.Footer_Content}>
                 <div className={Styles['Footer_Logo-And-slogan']}>
-                    <h1>3legant <span>.</span></h1> 
+                    <h1 onClick={() => newChangeLocation('/')}>3legant <span>.</span></h1> 
                     <span className={Styles.Footer_Palka}></span>
                     <h3>Gift & Decoration Store</h3>
                 </div>
                     
                 <nav className={Styles.Footer_Navigation}>
-                    <li>Home</li>
+                    <li onClick={() => newChangeLocation('/')}>Home</li>
                     <li>Shop</li>
                     <li>Products</li>
                     <li>Blog</li>
